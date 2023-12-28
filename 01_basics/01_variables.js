f93462ca34d73e23d761fcf08be29c6dab17a3a5
sumitrai9650 <145201172+sumitrai9650@users.noreply.github.com>
2023-12-28 10:07:05 +0000

    datatyes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "srai98788723@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 144553 not allowed
+
+accountEmail="hc@hc.gmail.com"
+accountPassword="21212121"
+accountCity="bengaluru"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"//treat all js code as newer version
+alert(3+3)//we are using nodejs not browser
+
+let name = "sumit"
+let age = 18
+let isLoggedIn = false/true
+
+// number => 2 to power 53
+// BigInt
+// String=>""
+// Boolean =>true/false
+// null => standalone value
+// undefined
+// Symbol => unique
+Object
+console.log(typeof name)
+/*
+primitive datatypes 
+boolean
+number 
+string
+symbol used for uniqueness
+null 
+undefined 
+boolean
+
+*/
